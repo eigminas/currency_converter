0397fb24469f2d255bd792cc79537f282203daac
Eigminas Slavinskas <e.slavinskas@newcastle.ac.uk>
2020-09-11 12:15:31 +0200

    final commit

--- back/server.js
+++ back/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const xmlparser = require("express-xml-bodyparser");
 const connectDB = require("./database/db");
 const cron = require("./middleware/cron");
-const path = require("path");
 
 const app = express();
 
@@ -27,15 +26,6 @@ app.get("/", (req, res) => {
 // define routes
 app.use("/api/data", require("./routes/api/data"));
 app.use("/api/operations", require("./routes/api/convert"));
-
-// Serve static assests if in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("front/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "front", "build", "index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
