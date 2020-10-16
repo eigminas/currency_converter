4c6b29e628ca9a56a946e8cf58daf5ac76cdc795
Eigminas Slavinskas <59088419+eigminas@users.noreply.github.com>
2020-10-16 16:41:07 +0300

    Fixed security issue

--- back/database/db.js
+++ back/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const db = "mongodb+srv://eigminas:862065851@currencies.v9xx6.mongodb.net/currencies?retryWrites=true&w=majority";
+  const db = "mongodb+srv://eigminas:<>@currencies.v9xx6.mongodb.net/currencies?retryWrites=true&w=majority";
   //const db = "mongodb://mongo:27017/currencies";
   try {
     await mongoose.connect(db, {
